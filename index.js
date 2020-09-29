e93557ec5187b478dec4aec12422ec2d61cb4152
kurishima-official <69749357+kurishima-official@users.noreply.github.com>
2020-09-29 16:33:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://bot-projevt.glitch.me"]
+const urls = ["https://dabkind.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
